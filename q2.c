3b56766a498bb473c9172d099316ea816c7fe9fb
TramDo <dothuytram@gmail.com>
2018-10-16 14:54:02 +0000

    test

--- q2.c
+++ q2.c
@@ -1,7 +1,51 @@
 /*Tram Do -215934276*/
 #include <stdio.h>
+
+int countDigits(int x, int d);
+char* testCountDigits(int x, int d, int result, char *msg);
+
 int main()
 {
-printf("");
+printf("%s\n",testCountDigits(123, 1, 1, "single digit test") );
+printf("%s\n",testCountDigits(10, 0, 1, "single 0 test") );
+printf("%s\n",testCountDigits(456456, 4, 2, "two fours") );
+printf("%s\n",testCountDigits(111111, 1, 6, "six 1's") );
+printf("%s\n",testCountDigits(0, 0, 1, "single zero test") );
+
 return 0;
 }
+
+int countDigits(int x, int d)
+{
+    int remainder, count;
+
+    count = 0;
+    while (x > 0)
+    {
+        remainder = x % 10;
+        if (remainder == d)
+        {
+            count++;
+        }
+        x = x/10;
+    }
+    if (x==0 && d == 0)
+    {
+        count = 1;
+    }
+    return count;
+}
+
+char* testCountDigits(int x, int d, int result, char *msg)
+{
+    int methodResult;
+    methodResult = countDigits(x,d);
+
+    if (methodResult == result)
+    {
+        return "test passed";
+    }
+    else
+        return "test failed";
+}
+
--- q3.c
+++ q3.c
@@ -1,9 +1,39 @@
 /*Tram Do - 215934276*/
 
 #include <stdio.h>
-int main (){
+void setTriple(float a, float b, float c, float *x, float *y, float *z);
+void addTriple(float a, float b, float c, float *x, float *y, float *z);
 
+int main ()
+{
+	float x, y, z;
+
+    setTriple(1, 2, 3, &x, &y, &z);
+    printf("x = %f, y = %f, z = %f\n", x,y,z);
+
+    addTriple(4, 5, 6, &x, &y, &z);
+    printf("x = %f, y = %f, z = %f\n", x,y,z);
+
+    addTriple(x, y, z, &x, &y, &z);
+    printf("x = %f, y = %f, z = %f\n", x,y,z);
 
 return 0;
+
+}
+
+void setTriple(float a, float b, float c, float *x, float *y, float *z)
+{
+	*x = a;
+	*y = b;
+	*z = c;
+
+}
+
+void addTriple(float a, float b, float c, float *x, float *y, float *z)
+{
+	*x = *x + a;
+	*y = *y + b;
+	*z = *z + c;
+
 
 }
